3e066cd7bcdceecdb4597c814dab21ddccdb077a
TomasFrattin <tomasfrattin@gmail.com>
2025-04-11 13:07:07 -0300

    fix(usersRoutes): fix get username

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ export const usersRouter = Router();
 usersRouter.post("/", UserController.createUser);
 usersRouter.get("/", UserController.getAllUsers);
 
-usersRouter.get("/:name", UserController.getUserByName);
+usersRouter.get("/:username", UserController.getUserByName);
 usersRouter.patch("/change-password", UserController.changePassword);
 usersRouter.delete("/:username", verifyToken, UserController.deleteUser);
 
